8f59ac7541686fb16475b46d1aab1a02a0d1a0d5
mahmoudashraf01 <dahmamahmoud87@gmail.com>
2025-04-14 20:34:40 +0200

    adding page builders on boarding styles

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Trendy Shop App',
       home: const ChoseProductOnBoardingScreen(),
     );
   }
 }
-
--- lib/view/screens/choose_product_on_boarding_screen.dart
+++ lib/view/screens/choose_product_on_boarding_screen.dart
@@ -1,10 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:trendy_shop/data/data_source/static/static.dart';
+import 'package:trendy_shop/utils/styles/text.dart';
 
 class ChoseProductOnBoardingScreen extends StatelessWidget {
   const ChoseProductOnBoardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        body: PageView.builder(
+          itemCount: onBoardingList.length,
+          itemBuilder: (context, index) {
+            return Stack(
+              children: [
+                ColorFiltered(
+                  colorFilter: ColorFilter.matrix(<double>[
+                    0.5,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0.5,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0.5,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    1,
+                    0,
+                  ]),
+
+                  child: Image.asset(
+                    onBoardingList[index].imgUrl!,
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  child: Column(
+                    children: [
+                      Center(
+                        child: Text(
+                          onBoardingList[index].title!,
+                          style: h2Bold,
+                        ),
+                      ),
+
+                      Padding(
+                        padding: EdgeInsets.only(
+                          top: MediaQuery.of(context).size.height * 0.3,
+                        ),
+                        child: Container(
+                          width: double.infinity,
+                          alignment: Alignment.center,
+                          child: Text(
+                            onBoardingList[index].body!,
+                            style: h4Bold,
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
   }
 }
