dffc64990dde898a4f2ad94f62524a633569bbde
mahmoudashraf01 <dahmamahmoud87@gmail.com>
2025-04-14 19:47:54 +0200

    adding onboarding model

---
+++ lib/core/constants/image_assets.dart
@@ -0,0 +1,6 @@
+class ImageAssets {
+  static const chooseProductImg = 'assets/images/choose_product_img.jpg';
+  static const easyPaymentImg = 'assets/images/easy_payment_img.jpg';
+  static const recieveOrederImg = 'assets/images/recieve_oreder_img.jpg';
+  static const trackOrderImg = 'assets/images/track_order_img.jpg';
+}
